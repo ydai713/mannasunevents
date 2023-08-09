bff4925d2d36c8d9292fe82360d11b2da8932c1f
ydai713 <yang.dai@klaviyo.com>
2023-08-08 20:09:03 -0700

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ['mannasunevents-images.s3.us-west-2.amazonaws.com', "scontent.cdninstagram.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+      },
+    ]
   },
 }
 
